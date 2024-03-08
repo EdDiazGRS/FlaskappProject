0fae3a004645bce4bd7563f657e8795ef3c5a179
EdDiazGRS <124111413+EdDiazGRS@users.noreply.github.com>
2024-03-08 17:13:44 -0500

    Update main.js

--- static/main.js
+++ static/main.js
@@ -1,1 +1,29 @@
 
+document.addEventListener("DOMContentLoaded", function() {
+  
+    document.getElementById("sports-link").addEventListener("click", function(event) {
+      event.preventDefault();
+      
+      // Make an AJAX request to the '/sports' route
+      fetch('/sports')
+        .then(response => response.text())
+        .then(data => {
+          // Replace the content of the main section with the response from the server
+          document.getElementById("main-content").innerHTML = data;
+        })
+        .catch(error => console.error('Error:', error));
+    });
+    
+    document.getElementById("knicks-link").addEventListener("click", function(event) {
+      event.preventDefault();
+      
+      // Make an AJAX request to the '/knicks' route
+      fetch('/knicks')
+        .then(response => response.text())
+        .then(data => {
+          document.getElementById("main-content").innerHTML = data;
+        })
+        .catch(error => console.error('Error:', error));
+    });
+  
+  });
